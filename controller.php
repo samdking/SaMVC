2c307fd277bf549881498c7caa88e1325c354e8a
= <=>
2012-01-22 11:17:45 +0000

    Extensive changes to rendering in Controller class.

--- controller.php
+++ controller.php
@@ -12,53 +12,50 @@ class Controller extends Base
       $class_name = $name . 'Controller';
       return new $class_name;
    }
-   
-   function hooks($uri)
+
+   function name()
    {
-      include self::find_file('hooks/sample_hook');
-      $hook = new Sample_hook;
-      $uri = $hook->edit($uri);
+      return str_replace('controller', '', strtolower(get_class($this)));
    }
    
    function run()
    {
       $uri = str_replace(URL_PATH, '', $_SERVER['REQUEST_URI']);
-      $this->hooks($uri);
+      $uri = Hook::apply('modify_uri', $uri);
       $parts = explode('/', $uri);
       $page = array_shift($parts);
       $method = $page? str_replace('-', '_', $page) : 'index';
-      $this->render('header');
-      if (method_exists($this, $method)) {
-         try {
-            call_user_func_array(array($this, $method), (array)$parts);
-            $this->render($this->view_file? $this->view_file : $method);
-         } catch (Exception $e) {
-            $this->error($method, $e);
-         }
-      } else {
-         $this->error($method);
-      }
-      $count = count(DB::init()->queries());
-      $this->render('footer', array(
-         'render_time' => round((microtime(false) - START_TIME), 5),
-         'query_count' => $count . ' ' . ($count == 1? 'query' : 'queries')
-      ));
-   }
-   
-   function error($method, Exception $e = NULL)
-   {
-      $this->render('error', array('method'=>$method, 'exception'=>$e));
-   }
-   
-   function render($name, $vars = array())
-   {
-      extract($vars);
-      extract($this->view_vars);
       try {
-         include self::find_file('views/' . $name);
+         $this->render_page($method, $parts);
       } catch (Exception $e) {
-         
+         Controller::load('error')->render_page('index', array($method, $e));   
       }
+   }
+   
+   function render_page($method, $parts)
+   {
+      $header_file = 'header';
+      $footer_file = 'footer';
+      $view_file = $this->name() . '/' . $method;
+      if (!method_exists($this, $method) OR !public_method($this, $method))
+         throw new Exception('No ' . $method . ' method exists in ' . get_class($this));
+      call_user_func_array(array($this, $method), (array)$parts);
+      extract($this->view_vars);
+      $this->header_file = self::find_file('views/' . $header_file);
+      $this->content_file = self::find_file('views/' . $view_file);
+      $this->footer_file = self::find_file('views/' . $footer_file);
+      $this->render_output();
+   }
+   
+   function render_output()
+   {
+      extract($this->view_vars);
+      include $header_file;
+      include $content_file;
+      $count = count(DB::init()->queries());
+      $render_time = round((microtime(false) - START_TIME), 5);
+      $query_count = $count . ' ' . ($count == 1? 'query' : 'queries');
+      include $footer_file;
    }
    
    function __set($property, $value)
