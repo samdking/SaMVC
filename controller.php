7971d0f8ddd7406750fd9fc24d0a15e3b99fe37e
= <=>
2012-01-22 22:43:41 +0000

    Controller class updated. More descriptive method names; better method distinction. Eventually need to add Router class to take some functionality away from controller

--- controller.php
+++ controller.php
@@ -5,11 +5,29 @@ class Controller extends Base
    protected $view_file = NULL;
    protected $view_vars = array();
    
-   static function load($name)
+   static function select()
    {
-      $file = 'controllers/' . $name;
-      include self::find_file($file);
-      $class_name = $name . 'Controller';
+      $uri = str_replace(URL_PATH, '', $_SERVER['REQUEST_URI']);
+      $uri = Hook::apply('modify_uri', $uri);
+      $parts = array_filter(explode('/', $uri));
+      $controller_name = empty($parts)? DEFAULT_CONTROLLER : $parts[0];
+      try {
+         Controller::route_controller($controller_name)->route_method($parts);
+      } catch (Exception $e) {
+         Controller::route_controller('error')->render_page('index', array($e));   
+      }      
+   }
+   
+   static function route_controller($name)
+   {   
+      $class_name = strtoupper($name) . 'Controller';
+      if (!class_exists($class_name)) {
+         try {
+            include self::find_file('controllers/' . $name);
+         } catch (Exception $e) {
+            return self::route_controller(DEFAULT_CONTROLLER);
+         }
+      }
       return new $class_name;
    }
 
@@ -18,37 +36,40 @@ class Controller extends Base
       return str_replace('controller', '', strtolower(get_class($this)));
    }
    
-   function run()
-   {
-      $uri = str_replace(URL_PATH, '', $_SERVER['REQUEST_URI']);
-      $uri = Hook::apply('modify_uri', $uri);
-      $parts = explode('/', $uri);
-      $page = array_shift($parts);
+   function route_method($parts)
+   {   
+      if (reset($parts) == $this->name())
+         array_shift($parts);
+      $page = reset($parts);
       $method = $page? str_replace('-', '_', $page) : 'index';
-      try {
-         $this->render_page($method, $parts);
-      } catch (Exception $e) {
-         Controller::load('error')->render_page('index', array($method, $e));   
+      if (!method_exists($this, $method) OR !public_method($this, $method)) {
+         if (!method_exists($this, 'catchall')) {
+            throw new Exception('No ' . $method . ' method exists in ' . get_class($this));
+         } else {
+            array_unshift($parts, 'catchall');
+            $method = 'catchall';
+         }
       }
+      array_shift($parts);
+      $this->render_page($method, $parts);
    }
    
    function render_page($method, $parts)
    {
       $header_file = 'header';
       $footer_file = 'footer';
-      $view_file = $this->name() . '/' . $method;
-      if (!method_exists($this, $method) OR !public_method($this, $method))
-         throw new Exception('No ' . $method . ' method exists in ' . get_class($this));
+      $this->view_file = $this->name() . '/' . $method;
       call_user_func_array(array($this, $method), (array)$parts);
       extract($this->view_vars);
       $this->header_file = self::find_file('views/' . $header_file);
-      $this->content_file = self::find_file('views/' . $view_file);
+      $this->content_file = self::find_file('views/' . $this->view_file);
       $this->footer_file = self::find_file('views/' . $footer_file);
       $this->render_output();
    }
    
    function render_output()
-   {
+   {      
+      $page_title = '';
       extract($this->view_vars);
       include $header_file;
       include $content_file;
