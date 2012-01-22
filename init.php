f1cb5f93ef984ed5e7c9d312359ad27ae0305c22
= <=>
2012-01-22 11:12:54 +0000

    A few tweaks to init.php

--- init.php
+++ init.php
@@ -8,11 +8,13 @@ define('APP_PATH', getcwd() . '/mvc/');
 $path = str_replace($_SERVER['DOCUMENT_ROOT'], '', getcwd());
 define('URL_PATH', '/' . str_replace($_SERVER['REQUEST_URI'], '', $path) . '/');
 
+require SYS_PATH . 'functions.php';
+require SYS_PATH . 'hook.php';
 require SYS_PATH . 'base.php';
 require SYS_PATH . 'model.php';
 require SYS_PATH . 'relation.php';
 require SYS_PATH . 'controller.php';
-foreach(glob(SYS_PATH . 'relations/*') as $file)
+foreach(glob(SYS_PATH . 'relations/*.php') as $file)
 	require $file;
 
 require SYS_PATH . 'inflector.php';
@@ -20,3 +22,6 @@ require SYS_PATH . 'db.php';
 
 $db['server'] = 'localhost';
 $db['handler'] = 'mysqli';
+
+if (DEFINED('HOOKS') && HOOKS == true)
+   Hook::load_hooks();
