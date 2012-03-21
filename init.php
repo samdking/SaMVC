9403e48f2da45d2f7580dfdc7f27e7a394f0de1e
= <=>
2012-03-21 00:08:02 +0000

    Added new router.php to init file

--- init.php
+++ init.php
@@ -1,16 +1,19 @@
 <?php
 
+define('CLI', PHP_SAPI === 'cli');
+
 define('START_TIME', microtime(false));
 
 define('SYS_PATH', dirname(__FILE__) . '/');
 define('APP_PATH', getcwd() . '/mvc/');
 
 $path = str_replace($_SERVER['DOCUMENT_ROOT'], '', getcwd());
-define('URL_PATH', '/' . str_replace($_SERVER['REQUEST_URI'], '', $path) . '/');
+define('URL_PATH', CLI? '/' : ('/' . str_replace($_SERVER['REQUEST_URI'], '', $path) . '/'));
 
 require SYS_PATH . 'functions.php';
 require SYS_PATH . 'hook.php';
 require SYS_PATH . 'base.php';
+require SYS_PATH . 'router.php';
 require SYS_PATH . 'model.php';
 require SYS_PATH . 'relation.php';
 require SYS_PATH . 'controller.php';
